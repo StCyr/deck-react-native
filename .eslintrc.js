8b7b7e3858bd42c802b02748a4b00eaa347fe98d
Cyrille Bollu <cyrille@bollu.be>
2021-10-11 13:53:58 +0200

    Install eslint + basic config
    Signed-off-by: Cyrille Bollu <cyrille@bollu.be>

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "commonjs": true,
+        "es2021": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:react/recommended"
+    ],
+    "parserOptions": {
+        "sourceType": "module",
+        "ecmaFeatures": {
+            "jsx": true
+        },
+        "ecmaVersion": 13
+    },
+    "plugins": [
+        "react"
+    ],
+    "rules": {
+    }
+};
