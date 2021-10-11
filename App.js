0489ed28d2132cb2022bb845ed510112825b65e9
Cyrille Bollu <cyrille@bollu.be>
2021-10-11 14:12:18 +0200

    fixes eslint on App.js
    Signed-off-by: Cyrille Bollu <cyrille@bollu.be>

--- App.js
+++ App.js
@@ -28,123 +28,123 @@ const Stack = createStackNavigator()
 class App extends React.Component {
 
 	state = {
-    	fontsLoaded: false,
-    	colorScheme: 'light',
-  	}
+		fontsLoaded: false,
+		colorScheme: 'light',
+	}
 
 	async loadFonts() {
-    	await Font.loadAsync({
-      		deck: require('./assets/fonts/deck/deck.ttf'),
-    	})
-    	this.setState({ fontsLoaded: true })
-  	}
+		await Font.loadAsync({
+			deck: require('./assets/fonts/deck/deck.ttf'),
+		})
+	this.setState({ fontsLoaded: true })
+	}
 
-  	constructor(props) {
-    	console.log('initialising app')
-    	super(props)
+	constructor(props) {
+		console.log('initialising app')
+		super(props)
 
 		// Force portrait mode on iPhones
 		if (Device.modelId.startsWith('iPhone')) {
 			ScreenOrientation.lockAsync(ScreenOrientation.OrientationLock.PORTRAIT_UP)
 		}
 
-     	// Register handler to catch Nextcloud's redirect after successfull login
-    	Linking.addEventListener('url', (url) => {this.handleRedirect(url)})
-  	}
+		// Register handler to catch Nextcloud's redirect after successfull login
+		Linking.addEventListener('url', (url) => {this.handleRedirect(url)})
+	}
 
-  	componentDidMount() {
-    	this.loadFonts()
-	    // get initial theme
-    	this.setState({ colorScheme: Appearance.getColorScheme()})
-	    this.props.setTheme(Appearance.getColorScheme());
-    	// register theme change subscription
-	    this._schemeSubscription = Appearance.addChangeListener(({ colorScheme }) => {
-    		this.setState({ colorScheme })
-	    	this.props.setTheme(colorScheme);
-    	});
-    	// Retrieve token from storage if available
-    	if (!env.expoDebug) {
-      		AsyncStorage.getItem('NCtoken').then(token => {
-        		if (token !== null) {
-          			console.log('token retrieved from asyncStorage', token)
-          			this.props.setToken('Basic ' + token)
-          			AsyncStorage.getItem('NCserver').then(server => {
-            			if (server !== null) {
-              				console.log('server retrieved from asyncStorage', server)
-              				this.props.setServer(server)
-            			}
-          			})
-        		}
-      		})
-    	} else {
-      		// Expo doesn't support registering URL protocol handler so we hardcode 
-      		// authentication parameters in environment.js file
-      		console.log('expo debug mode: setting token and server from hardcoded value')
-      		this.props.setToken(env.token)
-      		this.props.setServer(env.server)
-    	}
-  	}
+	componentDidMount() {
+		this.loadFonts()
+		// get initial theme
+		this.setState({ colorScheme: Appearance.getColorScheme()})
+		this.props.setTheme(Appearance.getColorScheme());
+		// register theme change subscription
+		this._schemeSubscription = Appearance.addChangeListener(({ colorScheme }) => {
+			this.setState({ colorScheme })
+			this.props.setTheme(colorScheme);
+		});
+		// Retrieve token from storage if available
+		if (!env.expoDebug) {
+			AsyncStorage.getItem('NCtoken').then(token => {
+				if (token !== null) {
+					console.log('token retrieved from asyncStorage', token)
+					this.props.setToken('Basic ' + token)
+					AsyncStorage.getItem('NCserver').then(server => {
+						if (server !== null) {
+							console.log('server retrieved from asyncStorage', server)
+							this.props.setServer(server)
+						}
+					})
+				}
+			})
+		} else {
+			// Expo doesn't support registering URL protocol handler so we hardcode 
+			// authentication parameters in environment.js file
+			console.log('expo debug mode: setting token and server from hardcoded value')
+			this.props.setToken(env.token)
+			this.props.setServer(env.server)
+		}
+	}
 
 	// Function to retrieve the device's token and save it after user logged in
-  	handleRedirect = async ({url}) => {
-    	if (url.startsWith('nc://login/server')) {
-        	console.log('Received the expected nc:// redirect', url)
-	        user = decodeURIComponent(url.substring(url.lastIndexOf('user:')+5, url.lastIndexOf('&')))
-    	    pwd = url.substring(url.lastIndexOf(':')+1)
-        	token = btoa(user + ':' + pwd)
-	        console.log('Persisting token in asyncStorage', token)
-    	    // TODO Use expo-secure-store to securely store the token
-        	AsyncStorage.setItem('NCtoken', token);  
-	        console.log('Saving token in store')
-    	    this.props.setToken('Basic ' + token)
-    	}
-  	}
+	handleRedirect = async ({url}) => {
+		if (url.startsWith('nc://login/server')) {
+			console.log('Received the expected nc:// redirect', url)
+			const user = decodeURIComponent(url.substring(url.lastIndexOf('user:')+5, url.lastIndexOf('&')))
+			const pwd = url.substring(url.lastIndexOf(':')+1)
+			const token = btoa(user + ':' + pwd)
+			console.log('Persisting token in asyncStorage', token)
+			// TODO Use expo-secure-store to securely store the token
+			AsyncStorage.setItem('NCtoken', token);  
+			console.log('Saving token in store')
+			this.props.setToken('Basic ' + token)
+		}
+	}
 
-  	render() {
-    	if(this.state.fontsLoaded) {
-	    	if (this.props.token.value === null || this.props.server.value === null) {
-    	    	// No token is stored yet, we need to get one
-		        return (
-        		    <NavigationContainer theme={this.state.colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-	            		<Stack.Navigator screenOptions={({navigation}) => {return {detachPreviousScreen: !navigation.isFocused()}}}>
-                			<Stack.Screen name="Home" component={Home} options={{ title: 'Login' }}/>
-                			<Stack.Screen name="Login" component={Login}/>
-              			</Stack.Navigator>
-            		</NavigationContainer>
-        		) 
-      		} else {
-        		return (
-          			<KeyboardAvoidingView style={{ flex: 1 }} behavior='padding'>
-            			<NavigationContainer theme={this.state.colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-			            	<Stack.Navigator screenOptions={({navigation}) => {return {detachPreviousScreen: !navigation.isFocused()}}}>
-                				<Stack.Screen name="AllBoards" component={AllBoards} />
-                				<Stack.Screen name="BoardDetails" component={BoardDetails} />
-                				<Stack.Screen name="CardDetails" component={Card} />
-                				<Stack.Screen name="NewCard" component={Card} />
-              				</Stack.Navigator>
-            			</NavigationContainer>
-          			</KeyboardAvoidingView>
-        		)
-      		}
-    	} else {
-      		return <AppLoading />
-    	}
-  	}
+	render() {
+		if(this.state.fontsLoaded) {
+			if (this.props.token.value === null || this.props.server.value === null) {
+				// No token is stored yet, we need to get one
+				return (
+					<NavigationContainer theme={this.state.colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+						<Stack.Navigator screenOptions={({navigation}) => {return {detachPreviousScreen: !navigation.isFocused()}}}>
+							<Stack.Screen name="Home" component={Home} options={{ title: 'Login' }}/>
+							<Stack.Screen name="Login" component={Login}/>
+						</Stack.Navigator>
+					</NavigationContainer>
+				) 
+			} else {
+				return (
+					<KeyboardAvoidingView style={{ flex: 1 }} behavior='padding'>
+						<NavigationContainer theme={this.state.colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+							<Stack.Navigator screenOptions={({navigation}) => {return {detachPreviousScreen: !navigation.isFocused()}}}>
+								<Stack.Screen name="AllBoards" component={AllBoards} />
+								<Stack.Screen name="BoardDetails" component={BoardDetails} />
+								<Stack.Screen name="CardDetails" component={Card} />
+								<Stack.Screen name="NewCard" component={Card} />
+							</Stack.Navigator>
+						</NavigationContainer>
+					</KeyboardAvoidingView>
+				)
+			}
+		} else {
+			return <AppLoading />
+		}
+	}
 
 }
 
 // Connect to store
 const mapStateToProps = state => ({
 	token: state.token,
-  	server: state.server,
-  	theme: state.theme,
+	server: state.server,
+	theme: state.theme,
 })
 const mapDispatchToProps = dispatch => (
 	bindActionCreators( {
-    	setServer,
-      	setToken,
-      	setTheme,
-  	}, dispatch)
+		setServer,
+		setToken,
+		setTheme,
+	}, dispatch)
 )
 export default connect(
 	mapStateToProps,
