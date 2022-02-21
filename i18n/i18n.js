7b08d280409f6a282bf0651fa53d2b11189abe1c
lollos78 <lorenzschuster@me.com>
2022-02-21 14:06:15 +0000

    Update i18n.js Added Italian (it) language

--- i18n/i18n.js
+++ i18n/i18n.js
@@ -1,10 +1,10 @@
 import * as Localization from 'expo-localization';
 import i18n from 'i18n-js';
-import { en, fr, ru, ua, ca, de } from './languages';
+import { en, fr, ru, ua, ca, de, it } from './languages';
 
 i18n.fallbacks = true;
 i18n.defaultLocale = 'en';
-i18n.translations = { en, fr, ru, ua, ca, de };
+i18n.translations = { en, fr, ru, ua, ca, de, it };
 i18n.locale = Localization.locale;
 
 export {i18n};
