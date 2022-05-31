0b3529752ccc57fc18e6590c488bc8981383007d
Cyrille Bollu <cyrille@bollu.be>
2022-05-31 22:39:19 +0200

    fixes a lint issue
    Signed-off-by: Cyrille Bollu <cyrille@bollu.be>

--- app.config.js
+++ app.config.js
@@ -3,10 +3,10 @@ export default ({ config }) => {
         ...config,
         ... {
             extra: {
-                // Fall back to development URL when not set
-                debug: process.env.DECK_DEBUG,
-                server: process.env.DECK_SERVER_URL,
-                token: process.env.DECK_SERVER_TOKEN
+                // Fall back to development URL when not set                 
+                debug: process.env.DECK_DEBUG,          // eslint-disable-line no-undef 
+                server: process.env.DECK_SERVER_URL,    // eslint-disable-line no-undef 
+                token: process.env.DECK_SERVER_TOKEN    // eslint-disable-line no-undef 
             }
         }
     }
